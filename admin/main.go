d8525fd56cefb4336d2d0ed758bbdda89aba5983
Miguel <miguelpccastro@gmail.com>
2024-10-01 10:19:05 -0300

    fix: check for db connection error

--- admin/main.go
+++ admin/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
+	"runtime/debug"
 
 	"github.com/tiqueteo/adminv2-mock-api/api/controllers"
 )
 
 func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Printf("Panic occurred: %v\nStack trace:\n%s", err, debug.Stack())
+			os.Exit(1) // Ensure the application exits after recovering from panic
+		}
+	}()
+
 	r := controllers.Init()
 	http.ListenAndServe(":8080", r)
 }

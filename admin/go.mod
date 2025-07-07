02d64da7dd5fd94006e07629fd30d73a5fd994d5
Miguel <mcastillo.ext@clorian.com>
2025-07-06 21:37:11 -0300

    feat: updated env variables via golang

--- admin/go.mod
+++ admin/go.mod
@@ -5,6 +5,7 @@ go 1.22
 require (
 	github.com/go-chi/cors v1.2.1
 	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/joho/godotenv v1.5.1
 	gorm.io/driver/postgres v1.6.0
 	gorm.io/gorm v1.25.10
 )
--- admin/main.go
+++ admin/main.go
@@ -6,10 +6,15 @@ import (
 	"os"
 	"runtime/debug"
 
+	"github.com/joho/godotenv"
 	"github.com/tiqueteo/adminv2-mock-api/api/controllers"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Printf("No %s file found or error loading it\n", ".env")
+	}
 	defer func() {
 		if err := recover(); err != nil {
 			log.Printf("Panic occurred: %v\nStack trace:\n%s", err, debug.Stack())
